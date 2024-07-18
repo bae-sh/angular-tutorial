2b315dce337947dec1b61029303156d11579a9fa
bae-sh <hyeon1293@gmail.com>
2024-07-18 15:53:46 +0900

    hello world

--- first-app-lesson-00/src/app/app.component.ts
+++ first-app-lesson-00/src/app/app.component.ts
@@ -1,12 +1,13 @@
+// app root 컴포넌트의 설명하는 부분
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [],
-  template: `<h1>Default</h1>`,
+  template: `<h1>Hello world!</h1>`,
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'default';
+  title = 'homes';
 }
